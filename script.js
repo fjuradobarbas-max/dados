6cb7e787d98bb772257ef44eeaacc00e66e66d57
fernando-jurado <fernando.jurado@vodafone.com>
2026-08-17 12:46:06 +0200

    funcional

--- script.js
+++ script.js
@@ -1,1 +1,74 @@
-document.body.style.backgroundColor = "red";
+const caras1 = ["ASES", "REYES", "DAMAS", "JOTAS", "NEGROS", "ROJOS"];
+const caras2 = ["ASES2", "REYES2", "DAMAS2", "JOTAS2", "NEGROS2", "ROJOS2"];
+
+let valor1 = "";
+let valor2 = "";
+
+function mostrarPantalla(id) {
+
+    document.querySelectorAll(".pantalla")
+        .forEach(p => p.classList.remove("activa"));
+
+    document.getElementById(id)
+        .classList.add("activa");
+}
+
+document.getElementById("btnInicio").onclick = function () {
+    mostrarPantalla("pantalla1");
+};
+
+document.getElementById("dado1").onclick = function () {
+
+    valor1 = caras1[Math.floor(Math.random() * 6)];
+
+    document.getElementById("resultado1").innerText = valor1;
+
+    document.getElementById("relanzar1").disabled = false;
+    document.getElementById("siguiente1").disabled = false;
+};
+
+document.getElementById("relanzar1").onclick = function () {
+    document.getElementById("dado1").click();
+};
+
+document.getElementById("siguiente1").onclick = function () {
+    mostrarPantalla("pantalla2");
+};
+
+document.getElementById("dado2").onclick = function () {
+
+    valor2 = caras2[Math.floor(Math.random() * 6)];
+
+    document.getElementById("resultado2").innerText = valor2;
+
+    document.getElementById("relanzar2").disabled = false;
+    document.getElementById("siguiente2").disabled = false;
+};
+
+document.getElementById("relanzar2").onclick = function () {
+    document.getElementById("dado2").click();
+};
+
+document.getElementById("siguiente2").onclick = function () {
+
+    document.getElementById("final1").innerText = valor1;
+    document.getElementById("final2").innerText = valor2;
+
+    mostrarPantalla("resumen");
+};
+
+document.getElementById("reinicio").onclick = function () {
+
+    valor1 = "";
+    valor2 = "";
+
+    document.getElementById("resultado1").innerText = "";
+    document.getElementById("resultado2").innerText = "";
+
+    document.getElementById("relanzar1").disabled = true;
+    document.getElementById("siguiente1").disabled = true;
+
+    document.getElementById("relanzar2").disabled = true;
+    document.getElementById("siguiente2").disabled = true;
+
+    mostrarPantalla
