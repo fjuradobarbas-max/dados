5a4dbc2f0a7dcb1e72d691d06b4e6fec6e5a5b57
fernando-jurado <fernando.jurado@vodafone.com>
2026-08-17 13:22:50 +0200

    cambio5

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const caras1 = ["ASES", "REYES", "DAMAS", "JOTAS", "NEGROS", "ROJOS"];
-const caras2 = ["ASES2", "REYES2", "DAMAS2", "JOTAS2", "NEGROS2", "ROJOS2"];
-
 let valor1 = "";
 let valor2 = "";
 
@@ -13,91 +10,10 @@ function mostrarPantalla(id) {
         .classList.add("activa");
 }
 
-document.getElementById("btnInicio").onclick = function () {
-    mostrarPantalla("pantalla1");
-};
-
-document.getElementById("dado1").onclick = function () {
-
-    animarDado("dado1");
-    
-    reproducirSonido();
-    
-    valor1 = caras1[Math.floor(Math.random() * 6)];
-
-    document.getElementById("resultado1").innerText = valor1;
-
-    document.getElementById("relanzar1").disabled = false;
-    document.getElementById("siguiente1").disabled = false;
-};
-
-document.getElementById("relanzar1").onclick = function () {
-    document.getElementById("dado1").click();
-};
-
-document.getElementById("siguiente1").onclick = function () {
-    mostrarPantalla("pantalla2");
-};
-
-document.getElementById("dado2").onclick = function () {
-
-    animarDado("dado2");
-    
-    reproducirSonido();
-
-    valor2 = caras2[Math.floor(Math.random() * 6)];
-
-    document.getElementById("resultado2").innerText = valor2;
-
-    document.getElementById("relanzar2").disabled = false;
-    document.getElementById("siguiente2").disabled = false;
-};
-
-document.getElementById("relanzar2").onclick = function () {
-    document.getElementById("dado2").click();
-};
-
-document.getElementById("siguiente2").onclick = function () {
-
-    document.getElementById("final1").innerText = valor1;
-    document.getElementById("final2").innerText = valor2;
-
-    mostrarPantalla("resumen");
-};
-
-document.getElementById("reinicio").onclick = function () {
-
-    valor1 = "";
-    valor2 = "";
-
-    document.getElementById("resultado1").innerText = "";
-    document.getElementById("resultado2").innerText = "";
-
-    document.getElementById("relanzar1").disabled = true;
-    document.getElementById("siguiente1").disabled = true;
-
-    document.getElementById("relanzar2").disabled = true;
-    document.getElementById("siguiente2").disabled = true;
-
-    mostrarPantalla("inicio");
-
-};
-
-function animarDado(idDado){
-
-    const dado = document.getElementById(idDado);
-
-    dado.classList.remove("animar-dado");
-
-    void dado.offsetWidth;
-
-    dado.classList.add("animar-dado");
-
-};
-
 function reproducirSonido() {
 
-    const ctx = new(window.AudioContext || window.webkitAudioContext)();
+    const ctx =
+        new (window.AudioContext || window.webkitAudioContext)();
 
     const osc = ctx.createOscillator();
     const gain = ctx.createGain();
@@ -105,11 +21,105 @@ function reproducirSonido() {
     osc.connect(gain);
     gain.connect(ctx.destination);
 
-    osc.frequency.value = 200;
-
-    gain.gain.value = 0.2;
+    osc.frequency.value = 180;
+    gain.gain.value = 0.15;
 
     osc.start();
 
-    osc.stop(ctx.currentTime + 0.2);
+    osc.stop(ctx.currentTime + 0.25);
 }
+
+function lanzarDado(idDado, tipo) {
+
+    const dado =
+        document.getElementById(idDado);
+
+    reproducirSonido();
+
+    if (navigator.vibrate) {
+        navigator.vibrate(250);
+    }
+
+    const resultado =
+        Math.floor(Math.random() * 6);
+
+    dado.classList.remove("lanzando");
+
+    void dado.offsetWidth;
+
+    dado.classList.add("lanzando");
+
+    setTimeout(() => {
+
+        dado.classList.remove("lanzando");
+
+        let nombre = "";
+
+        switch (resultado) {
+
+            case 0:
+                dado.style.transform =
+                    "rotateX(0deg) rotateY(0deg)";
+                nombre = tipo === 1 ? "ASES" : "ASES2";
+                break;
+
+            case 1:
+                dado.style.transform =
+                    "rotateY(180deg)";
+                nombre = tipo === 1 ? "REYES" : "REYES2";
+                break;
+
+            case 2:
+                dado.style.transform =
+                    "rotateY(-90deg)";
+                nombre = tipo === 1 ? "DAMAS" : "DAMAS2";
+                break;
+
+            case 3:
+                dado.style.transform =
+                    "rotateY(90deg)";
+                nombre = tipo === 1 ? "JOTAS" : "JOTAS2";
+                break;
+
+            case 4:
+                dado.style.transform =
+                    "rotateX(-90deg)";
+                nombre = tipo === 1 ? "NEGROS" : "NEGROS2";
+                break;
+
+            case 5:
+                dado.style.transform =
+                    "rotateX(90deg)";
+                nombre = tipo === 1 ? "ROJOS" : "ROJOS2";
+                break;
+        }
+
+        if (tipo === 1) {
+
+            valor1 = nombre;
+
+            document.getElementById("relanzar1").disabled = false;
+            document.getElementById("siguiente1").disabled = false;
+
+        } else {
+
+            valor2 = nombre;
+
+            document.getElementById("relanzar2").disabled = false;
+            document.getElementById("siguiente2").disabled = false;
+        }
+
+    }, 1800);
+}
+
+document.getElementById("btnInicio").onclick = function () {
+
+    mostrarPantalla("pantalla1");
+};
+
+document.getElementById("dado1").onclick = function () {
+
+    lanzarDado("dado1", 1);
+};
+
+document.getElementById
